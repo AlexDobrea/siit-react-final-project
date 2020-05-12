0a146e3f5e87244a944864e143c765626efd542e
Alexandru Dobrea <ale_dobrea@yahoo.com>
2020-05-12 16:46:56 +0300

    Update header

--- react-final-project/src/components/shared/header/Header.js
+++ react-final-project/src/components/shared/header/Header.js
@@ -16,29 +16,30 @@ function Header() {
     }
 
     return (
-        <nav className="app-header">
-            <ul >
-                <li><img className="logo-header" src={ logo } alt="logo"/></li>
-                <li><NavLink exact to="/" activeClassName="active">Photos</ NavLink></li>
-                <li><NavLink to="/videos" activeClassName="active">Videos</NavLink></li>
-                <li className="app-header-auth">
-                    { ( user  ? 
-                        <>
-                            <NavLink to="/addVideo" activeClassName="active"> Add video</NavLink>
-                            <a href="/" onClick={ handleLogout }>
-                                Logout { user }
-                            </a>
-                        </>
-                    :
-                        <>
-                            <NavLink to="/login" activeClassName="active">Login</NavLink>
-                            <NavLink to="/register" activeClassName="active">Register</NavLink>
-                        </>
-                    )}
-                </li>
-            </ul>
-
-        </nav>
+        <header className="app-header">
+            <nav>
+                <ul >
+                    <li><img className="logo-header" src={ logo } alt="logo"/></li>
+                    <li><NavLink exact to="/" activeClassName="active">Photos</ NavLink></li>
+                    <li><NavLink to="/videos" activeClassName="active">Videos</NavLink></li>
+                    <li className="app-header-auth">
+                        { ( user  ? 
+                            <>
+                                <NavLink to="/addVideo" activeClassName="active"> Add video</NavLink>
+                                <a href="/" onClick={ handleLogout }>
+                                    Logout, { user }
+                                </a>
+                            </>
+                        :
+                            <>
+                                <NavLink to="/login" activeClassName="active">Login</NavLink>
+                                <NavLink to="/register" activeClassName="active">Register</NavLink>
+                            </>
+                        )}
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
